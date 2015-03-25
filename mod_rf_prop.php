3c30af79bb8489d6e3606b3f8e9a756af125b81c
Tighe Kuykendall <tighe@buoy.net>
2015-03-25 08:39:06 -0400

    Removed 1hr Stability.

--- mod_rf_prop.php
+++ mod_rf_prop.php
@@ -4,7 +4,7 @@
  * mod_rf_proagations Module Entry Point
  * 
  * @package		RF Propagations for Joomla! 3.x
- * @version		$Id: mod_rf_prop.php 2015-03-22 12:45:00Z T Kuykendall NK4I $
+ * @version		$Id: mod_rf_prop.php v1.2 $
  * @author		T Kuykendall NK4I
  * @copyright	Copyright (C) 2015 - T Kuykendall NK4I
  * @license		GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
